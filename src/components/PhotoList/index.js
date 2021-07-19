d5823dd6b13980b4010b2cad2cbeba5543feb208
Ronan-Codes <galv.ronan@gmail.com>
2021-07-19 01:36:40 -0400

    Utilize Module Project as base of react portfolio project

---
+++ src/components/PhotoList/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Modal from '../Modal';
+
+const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
+  const [photos] = useState([
+    {
+      name: 'Grocery aisle',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Grocery booth',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Building exterior',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Restaurant table',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Cafe interior',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    }
+  ]);
+
+  const currentPhotos = photos.filter((photo) => photo.category === category);
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div>
+      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
+      <div className="flex-row">
+        {currentPhotos.map((image, i) => (
+          <img
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
+            key={image.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoList;
