7d1310cf3929a07b653d7c4150bab0075787fff0
Ronan-Codes <galv.ronan@gmail.com>
2021-07-19 06:13:36 -0400

    Updates

---
+++ precache-manifest.70c9efda5fd235ac16ca4a99801cd052.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65387a0055cc1b21683a66c607612547",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "46e17c26fa12b7d518ce",
+    "url": "/react-portfolio/static/css/main.7bcbee03.chunk.css"
+  },
+  {
+    "revision": "47658d797c9b71864f93",
+    "url": "/react-portfolio/static/js/2.c7e06ab6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.c7e06ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e17c26fa12b7d518ce",
+    "url": "/react-portfolio/static/js/main.ffcf2969.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "e01472543f2b6f7812ab8b84f3df5e5f",
+    "url": "/react-portfolio/static/media/0.e0147254.jpg"
+  },
+  {
+    "revision": "4252e23b0f57436fb1bda061723a3667",
+    "url": "/react-portfolio/static/media/1.4252e23b.jpg"
+  },
+  {
+    "revision": "301f2e9adfc98dc3eb1aab7b1593c4fc",
+    "url": "/react-portfolio/static/media/2.301f2e9a.jpg"
+  },
+  {
+    "revision": "af8a0972fe7cba99561eab154b643e9f",
+    "url": "/react-portfolio/static/media/3.af8a0972.jpg"
+  },
+  {
+    "revision": "9205849cc5fa9fd730ee8ebbccd12825",
+    "url": "/react-portfolio/static/media/4.9205849c.jpg"
+  },
+  {
+    "revision": "5d7bb60215a201fe182548f7b6d98bbe",
+    "url": "/react-portfolio/static/media/5.5d7bb602.jpg"
+  },
+  {
+    "revision": "6f7c959eef5bd25bcad2dcd4b7927c84",
+    "url": "/react-portfolio/static/media/ronanGalvezResume.6f7c959e.pdf"
+  },
+  {
+    "revision": "28302ec17b5e64baad11fdd7a98af2be",
+    "url": "/react-portfolio/static/media/temp-portrait.28302ec1.jpeg"
+  }
+]);
