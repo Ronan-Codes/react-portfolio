88788c16aad835668d2fd640131bceee78b078c8
Ronan-Codes <galv.ronan@gmail.com>
2022-05-25 10:16:35 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.523630b01c9f62fb7bb3ebb3d70fb249.js"
+  "/react-portfolio/precache-manifest.114d979dbcdbb9f55740e234826776dd.js"
 );
 
 self.addEventListener('message', (event) => {
